66a27d20692e42d09b44198fe4034c1c5913b50e
APpLePeN <glaziccc@gmail.com>
2022-04-06 22:48:41 +0300

    Fixes

--- scripts/vscripts/hack/item_random_get_ability_onlvl.lua
+++ scripts/vscripts/hack/item_random_get_ability_onlvl.lua
@@ -10,47 +10,22 @@ function OnSpellStart( keys )
 		return
 	end
 	if hero:IsRealHero() then
---[[
-		local ability4 = hero:GetAbilityByIndex(3)
-		local ability5 = hero:GetAbilityByIndex(4)
-		local ability7 = hero:GetAbilityByIndex(6)
-		local ability8 = hero:GetAbilityByIndex(7)
-]]
-		local slotId = -1
---[[
-		if ability4 then slotId = 4 end
-		if ability5 then slotId = 6 end
-		if ability7 then slotId = 8 end
-		if ability8 then slotId = nil end
-		if ability8 == nil or ability8:GetName() == "generic_hidden" then slotId = 7 end
-		if ability7 == nil or ability7:GetName() == "generic_hidden" then slotId = 6 end
-		if ability5 == nil or ability5:GetName() == "generic_hidden" then slotId = 4 end
-		if ability4 == nil or ability4:GetName() == "generic_hidden" then slotId = 3 end
-		if ability8 == nil then slotId = 7 end
-		if ability7 == nil then slotId = 6 end
-]]
-		if hero:GetAbilityByIndex(3) == nil then
-			slotId = -1
-		elseif hero:GetAbilityByIndex(3):GetName() == "generic_hidden" then
-			slotId = 3
-		elseif hero:GetAbilityByIndex(4):GetName() == "generic_hidden" then
-			slotId = 4
-		end
 
---[[
-		if IsInToolsMode() then
-			print("ability8 :" .. ability8:GetName())
-			print("ability7 :" .. ability7:GetName())
-			print("ability5 :" .. ability5:GetName())
-			print("ability4 :" .. ability4:GetName())
-			print("newAbility slotID:" .. slotId)
+		local GenericSlots = 0
+		for i = 0, DOTA_MAX_ABILITIES - 1 do
+			local abil = caster:GetAbilityByIndex(i)
+			if abil then
+				if abil:GetAbilityName() == "generic_hidden" then
+					GenericSlots = GenericSlots + 1
+				end
+			end
 		end
-]]
-		if slotId > -1 then
-			local oldAbility = hero:GetAbilityByIndex(slotId)
-			if oldAbility then
-				print("oldAbility:" .. oldAbility:GetName())
-				hero:RemoveAbilityByHandle(oldAbility)
+		if GenericSlots > 0 then
+			caster:RemoveAbility("generic_hidden")
+			if GenericSlots > 1 then
+				for i = 1, GenericSlots - 1 do
+					caster:AddAbility("generic_hidden")
+				end
 			end
 		end
 
@@ -60,22 +35,22 @@ function OnSpellStart( keys )
 				if hAbility and not hAbility:IsNull() and not hAbility:IsCosmetic( nil ) then
 					local hAbility_name = hAbility:GetAbilityName()
 					if hAbility_name == "ability_capture" then
-						print("Remove: " .. hAbility_name)
+--						print("Remove: " .. hAbility_name)
 						if caster and caster:HasAbility(hAbility_name) then
 							caster:RemoveAbility(hAbility_name)
-						end	
-					end	
-				end	
+						end
+					end
+				end
 			end
-		end	
+		end
 
 		local exclude_table = {
 			faceless_void_backtrack = true,
 			phantom_reflex = true,
-		}		
+		}
 		local found_valid_ability = false
 		while not found_valid_ability do
-			local newAbilityName = GetRandomAbilityName(hero)											
+			local newAbilityName = GetRandomAbilityName(hero)
 			if not hero:HasAbility(newAbilityName) then
 				if exclude_table[newAbilityName] then
 					if hero:HasAbility("phantom_reflex") or hero:HasAbility("faceless_void_backtrack") then
@@ -84,19 +59,19 @@ function OnSpellStart( keys )
 						ability:OnSpellStart()
 						return
 					end
-				end 
+				end
 				if hero:HasAbility("shadow_demon_custom_hyperactivity") then
 					if newAbilityName == "obs_replay" then
 						--print(newAbilityName .. " nope2")
-						ability:OnSpellStart() 
-						return							
-					end	
+						ability:OnSpellStart()
+						return
+					end
 				end
 				if newAbilityName == "clinkz_infernal_breath" then
 					if hero:HasAbility("mjz_phantom_assassin_coup_de_grace") then
-						ability:OnSpellStart() 
-						return							
-					end	
+						ability:OnSpellStart()
+						return
+					end
 				end
 				if newAbilityName == "mjz_clinkz_death_pact" then
 					if hero:HasAbility("mjz_clinkz_soul_pact") then
@@ -104,105 +79,101 @@ function OnSpellStart( keys )
 						return
 					end
 				end
-				local newAbility = hero:AddAbility(newAbilityName)	  
-				print("newAbility:" .. newAbilityName)  
---				if slotId > -1 then						  
---					newAbility:SetAbilityIndex(slotId)
---				end
+				local newAbility = hero:AddAbility(newAbilityName)
+--				print("newAbility:" .. newAbilityName)
 				hero:SetAbilityPoints(hero:GetAbilityPoints() + 1)
 				found_valid_ability = true
 				hero:RemoveItem(ability)
 				hero:ModifyGold(321, true, 0)
-				return true													
-			end													
+				return true
+			end
 		end
 	end
 end
 
-function GetRandomAbilityName( hero )				 
+function GetRandomAbilityName( hero )
 	local abilityList = {
-		"beastmaster_inner_beast",	  -- 兽王	野性之心
-		--"skeleton_king_vampiric_aura",	  --骷髅王2
-		"skeleton_king_mortal_strike",	  --骷髅王3
-		"spirit_breaker_greater_bash",			--发条2
-		--"drow_ranger_marksmanship",	 --大地之灵2
-		"sven_great_cleave",				-- sven 2
-		"mjz_kunkka_tidebringer",			   -- 船长 2
-		"mjz_pudge_flesh_heap",				 -- 屠夫 3
-		"mjz_obsidian_destroyer_essence_aura",					  -- 全能 3
-		--"shredder_reactive_armor",		  -- 伐木机 3
-		"mjz_night_stalker_hunter_in_the_night",				  -- 潮汐 1
-		"phantom_assassin_blur",		  -- 潮汐 2
-		"tidehunter_anchor_smash",		  -- 潮汐 3
-		"mjz_tiny_craggy_exterior",   -- 军团 3
-		"mjz_omniknight_degen_aura",		-- 夜魔 3
-		"mjz_vengefulspirit_vengeance",			-- 老奶奶 2
-		"mjz_troll_warlord_fervor",			-- 老奶奶 3
-		"chaos_knight_chaos_strike",		-- 混沌3
-		"mars_bulwark",			  -- 猛犸 1
-		"magnataur_empower",				-- 猛犸 2
-		"legion_commander_custom_duel",				 -- 猛犸 3
-		"alchemist_chemical_rage",				 -- 马尔斯 2
-		"mjz_dragon_knight_dragon_blood",	   -- 龙骑3
-		"mars_bulwark",					-- 小骷髅 扫射
-		"juggernaut_healing_ward",		  -- 剑圣2
-		"mjz_juggernaut_blade_fury",		   -- 剑圣3
-		"templar_assassin_psi_blades",	  -- TA 3
-		"mjz_vengefulspirit_vengeance",	  -- VS 3
-		"monkey_king_custom_jingu_mastery",		  -- 小娜迦 1
-		"alchemist_goblins_greed",			  -- 小娜迦 3
-		"mjz_omniknight_repel",		   -- 影魔 4
-		"lycan_feral_impulse",			  -- 影魔 5
-		"antimage_custom_counterspell",			  -- 敌法 1 crash with snowball 
-		"mjz_ursa_overpower",				   -- 拍拍熊 2
-		"ursa_fury_swipes",				 -- 拍拍熊 3
-		"lich_custom_cold_soul",				  -- 火枪 3
-		"mjz_invoker_magic_master",		   -- 飞机 3
-		--"medusa_split_shot",				-- 美杜莎 1
-		"medusa_mana_shield",			   -- 美杜莎 3
-		"mjz_broodmother_insatiable_hunger",					  -- 白虎 3
-		--"abyssal_underlord_atrophy_aura",			 -- 滚滚 3
-		"meepo_ransack",					-- 米波 3
-		"bloodseeker_thirst",		   -- 蚂蚁 3	bug
-		"faceless_void_time_lock",		  -- 虚空 3
-		"mjz_lina_laguna_blade",			-- 血魔 1
-		--"luna_lunar_blessing",			  -- 露娜 3
-		"elder_titan_natural_order",				 -- 蝙蝠 3
-		"nyx_assassin_custom_vendetta",			 -- 先知 2
-		--"skywrath_mage_ancient_seal",	   -- 天怒 3
-		"mjz_axe_counter_helix",		-- 冰龙 1 crash on snowball
-		--"dark_willow_bedlam",					  -- 巫医 2
-		"alchemist_power_of_gold2",				 -- 巫妖 2
-		"alchemist_power_of_gold",		  -- 干扰者 3
-		"rubick_arcane_supremacy",		  -- 拉比克 3
-		"jakiro_liquid_fire",			   -- 双头龙 3
-		"obsidian_destroyer_arcane_orb",	-- 黑鸟 1
-		"mjz_crystal_maiden_brilliance_aura",   -- 冰女 3
-		"mystic_dragon_endless_wisdom",	 -- 沉默 1
-		"imba_phantom_assassin_coup_de_grace",				-- 女王 2
-		--"dazzle_good_juju",				  -- 丽娜 3
-		"ancient_apparition_chilling_touch",	-- 冰魂 3
-		"roshan_inherit_buff_datadriven",			-- 蓝猫 3
-		"ogre_magi_multicast_n",			  -- 蓝胖 3
-		"enchantress_impetus",			  -- 小鹿 1
-		"dark_seer_custom_ion_shell",				  -- 黑贤 3
-		"mjz_centaur_return",				   -- 人马 3
-		"huskar_berserkers_blood",		  -- 哈斯卡 3
-
+		"beastmaster_inner_beast",
+		--"skeleton_king_vampiric_aura",
+		"skeleton_king_mortal_strike",
+		"spirit_breaker_greater_bash",
+		--"drow_ranger_marksmanship",
+		"sven_great_cleave",
+		"mjz_kunkka_tidebringer",
+		"mjz_pudge_flesh_heap",
+		"mjz_obsidian_destroyer_essence_aura",
+		--"shredder_reactive_armor",
+		"mjz_night_stalker_hunter_in_the_night",
+		"phantom_assassin_blur",
+		"tidehunter_anchor_smash",
+		"mjz_tiny_craggy_exterior",
+		"mjz_omniknight_degen_aura",
+		"mjz_vengefulspirit_vengeance",
+		"mjz_troll_warlord_fervor",
+		"chaos_knight_chaos_strike",
+		"mars_bulwark",
+		"magnataur_empower",
+		"legion_commander_custom_duel",
+		"alchemist_chemical_rage",
+		"mjz_dragon_knight_dragon_blood",
+		"mars_bulwark",
+		"juggernaut_healing_ward",
+		"mjz_juggernaut_blade_fury",
+		"templar_assassin_psi_blades",
+		"mjz_vengefulspirit_vengeance",
+		"monkey_king_custom_jingu_mastery",
+		"alchemist_goblins_greed",
+		"mjz_omniknight_repel",
+		"lycan_feral_impulse",
+		"antimage_custom_counterspell",
+		"mjz_ursa_overpower",
+		"ursa_fury_swipes",
+		"lich_custom_cold_soul",
+		"mjz_invoker_magic_master",
+		--"medusa_split_shot",
+		"medusa_mana_shield",
+		"mjz_broodmother_insatiable_hunger",
+		--"abyssal_underlord_atrophy_aura",
+		"meepo_ransack",
+		"bloodseeker_thirst",
+		"faceless_void_time_lock",
+		"mjz_lina_laguna_blade",
+		--"luna_lunar_blessing",
+		"elder_titan_natural_order",
+		"nyx_assassin_custom_vendetta",
+		--"skywrath_mage_ancient_seal",
+		"mjz_axe_counter_helix",
+		--"dark_willow_bedlam",
+		"alchemist_power_of_gold2",
+		"alchemist_power_of_gold",
+		"rubick_arcane_supremacy",
+		"jakiro_liquid_fire",
+		"obsidian_destroyer_arcane_orb",
+		"mjz_crystal_maiden_brilliance_aura",
+		"mystic_dragon_endless_wisdom",
+		"imba_phantom_assassin_coup_de_grace",
+		--"dazzle_good_juju",
+		"ancient_apparition_chilling_touch",
+		"roshan_inherit_buff_datadriven",
+		"ogre_magi_multicast_n",
+		"enchantress_impetus",
+		"dark_seer_custom_ion_shell",
+		"mjz_centaur_return",
+		"huskar_berserkers_blood",
 		--"antimage_spell_shield",	 
-		"faceless_void_backtrack",		 --3
-		--"bane_enfeeble",		   -- 2
-		"phantom_lancer_phantom_edge",	   -- 1
-		"ryze_arcane_mastery",	 --4
-		"juggernaut_blade_dance_lua",			  --  魔抗光环 
-		"vengefulspirit_command_aura_lua",	  -- 熊怪 迅捷光环
-		"omniknight_repel_lua",			 -- 坚韧光环
-		"doom_devour_lua",	 -- 龙肤光环 没有光环
-		"lion_custom_finger_of_death",	   -- 溅射攻击
-		"big_thunder_lizard_wardrums_aura",   --战鼓光环
+		"faceless_void_backtrack",
+		--"bane_enfeeble",
+		"phantom_lancer_phantom_edge",
+		"ryze_arcane_mastery",
+		"juggernaut_blade_dance_lua",
+		"vengefulspirit_command_aura_lua",
+		"omniknight_repel_lua",
+		"doom_devour_lua",
+		"lion_custom_finger_of_death",
+		"big_thunder_lizard_wardrums_aura",
 		"phantom_reflex",
-		"custom_aphotic_shield",					  -- ROSHAN 重击
-		"mjz_general_megamorph",			--巨大化
+		"custom_aphotic_shield",
+		"mjz_general_megamorph",
 		"ogre_magi_multicast_lua",
 		"juggernaut_omni_slash",
 		"lycan_feral_impulse",
@@ -261,5 +232,5 @@ function GetRandomAbilityName( hero )
 		"brewmaster_drunken_brawler",
 	}
 	local randomIndex = RandomInt(1, #abilityList)
-	return abilityList[randomIndex]   
+	return abilityList[randomIndex]
 end
--- scripts/vscripts/hack/item_random_get_ability_test.lua
+++ scripts/vscripts/hack/item_random_get_ability_test.lua
@@ -4,61 +4,38 @@ function OnSpellStart( keys )
     local ability = keys.ability
     local unit = keys.unit
     local hero = caster
+	if caster:IsNull() then return end
     if hero:HasModifier("modifier_arc_warden_tempest_double") then
         ability:SetActivated(false)
 		return
     end
     if hero:IsRealHero() then
---[[
-		local ability4 = hero:GetAbilityByIndex(3)
-		local ability5 = hero:GetAbilityByIndex(4)
-		local ability7 = hero:GetAbilityByIndex(6)
-		local ability8 = hero:GetAbilityByIndex(7)
-]]
-		local slotId = -1
---[[
-		if ability4 then slotId = 4 end
-		if ability5 then slotId = 6 end
-		if ability7 then slotId = 8 end
-		if ability8 then slotId = nil end
-		if ability8 == nil or ability8:GetName() == "generic_hidden" then slotId = 7 end
-		if ability7 == nil or ability7:GetName() == "generic_hidden" then slotId = 6 end
-		if ability5 == nil or ability5:GetName() == "generic_hidden" then slotId = 4 end
-		if ability4 == nil or ability4:GetName() == "generic_hidden" then slotId = 3 end
-		if ability8 == nil then slotId = 7 end
-		if ability7 == nil then slotId = 6 end
-]]
-		if hero:GetAbilityByIndex(3) == nil then
-			slotId = -1
-		elseif hero:GetAbilityByIndex(3):GetName() == "generic_hidden" then
-			slotId = 3
-		elseif hero:GetAbilityByIndex(4):GetName() == "generic_hidden" then
-			slotId = 4
-		end
 
---[[
-		if IsInToolsMode() then
-			print("ability8 :" .. ability8:GetName())
-			print("ability7 :" .. ability7:GetName())
-			print("ability5 :" .. ability5:GetName())
-			print("ability4 :" .. ability4:GetName())
-			print("newAbility slotID:" .. slotId)
-		end
-]]
-		if slotId > -1 then
-			local oldAbility = hero:GetAbilityByIndex(slotId)
-			if oldAbility then
-				print("oldAbility:" .. oldAbility:GetName())
-				hero:RemoveAbilityByHandle(oldAbility)
+		local GenericSlots = 0
+		for i = 0, DOTA_MAX_ABILITIES - 1 do
+			local abil = caster:GetAbilityByIndex(i)
+			if abil then
+				if abil:GetAbilityName() == "generic_hidden" then
+					GenericSlots = GenericSlots + 1
+				end
 			end
 		end
+		if GenericSlots > 0 then
+			caster:RemoveAbility("generic_hidden")
+			if GenericSlots > 1 then
+				for i = 1, GenericSlots - 1 do
+					caster:AddAbility("generic_hidden")
+				end
+			end
+		end
+
         local exclude_table = {
             faceless_void_backtrack = true,
             phantom_reflex = true,
-        }        
+        }
         local found_valid_ability = false
         while not found_valid_ability do
-            local newAbilityName = GetRandomAbilityName(hero)                                            
+            local newAbilityName = GetRandomAbilityName(hero)
             if not hero:HasAbility(newAbilityName) then
                 if exclude_table[newAbilityName] then
                     if hero:HasAbility("phantom_reflex") or hero:HasAbility("faceless_void_backtrack") then
@@ -68,29 +45,26 @@ function OnSpellStart( keys )
                 if hero:HasAbility("shadow_demon_custom_hyperactivity") then
                     if newAbilityName == "obs_replay" then
                         print(newAbilityName .. " nope2")
-                        --ability:OnSpellStart() 
-                        return                            
-                    end    
-                end                                
-                local newAbility = hero:AddAbility(newAbilityName)      
-                print("newAbility:" .. newAbilityName)  
---				if slotId > -1 then                          
---					newAbility:SetAbilityIndex(slotId)
---				end
+                        --ability:OnSpellStart()
+                        return
+                    end
+                end
+                local newAbility = hero:AddAbility(newAbilityName)
+                print("newAbility:" .. newAbilityName)
                 hero:SetAbilityPoints(hero:GetAbilityPoints() + 5)
                 found_valid_ability = true
                 hero:RemoveItem(ability)
                 hero:ModifyGold(19213, true, 0)
-                return true                                                   
+                return true
             end
-            break                                                    
+            break
         end
     end
 end
 
-function GetRandomAbilityName( hero )                 
+function GetRandomAbilityName( hero )
     local abilityList = {
-        "ogre_magi_multicast_n",      -- 兽王    野性之心
+        "ogre_magi_multicast_n",
         "ogre_magi_multicast_lua",
         --"aghanim_blink2",
         "ursa_fury_swipes",
@@ -118,5 +92,5 @@ function GetRandomAbilityName( hero )
         
     }
     local randomIndex = RandomInt(1, #abilityList)
-    return abilityList[randomIndex]   
+    return abilityList[randomIndex]
 end
