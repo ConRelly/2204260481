886c1ae4b03b262391051066d6eb69cc4698b328
ConRelly <bobby_bossull@yahoo.com>
2022-05-01 02:27:55 +0300

    Final update

--- scripts/vscripts/items/item_custom_seer_stone.lua
+++ scripts/vscripts/items/item_custom_seer_stone.lua
@@ -25,6 +25,15 @@ function item_custom_seer_stone:OnSpellStart()
 	true_sight:AddNewModifier(caster, self, "modifier_item_custom_seer_stone_true_sight", {duration = duration})
 
 	AddFOWViewer(caster:GetTeamNumber(), self:GetCursorPosition(), radius, duration, false)
+	if caster:HasAbility("bounty_hunter_track") and caster:FindAbilityByName("bounty_hunter_track"):IsTrained() then
+		local track = caster:FindAbilityByName("bounty_hunter_track")
+		local enemies = FindUnitsInRadius(caster:GetTeamNumber(), self:GetCursorPosition(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_CLOSEST, false)
+		for _,enemy in pairs(enemies) do
+			-- apply track
+			caster:SetCursorCastTarget(enemy)
+			track:OnSpellStart()		
+		end
+	end	
 end
 
 
